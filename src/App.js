392b01734c87d9b296bb73379ec929b113b3685e
Boris <boris.pozzo@gmail.com>
2017-09-24 00:12:59 +0200

    add quizz

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { questions } from "./questions.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { index: 0 }
+  }
+
+  increment = () => {
+    this.setState({ index: this.state.index + 1 })
+    console.log('increment')
+  }
   render() {
+    const { index } = this.state;
+    let q = [questions[index]];
+    const quizz = q.map((el, i) => {
+      return (
+        <div>
+          <h3 key={i}>{el.question}</h3>
+          <div>
+            <p>{el.reponse.one}</p>
+            <p>{el.reponse.two}</p>
+            <p>{el.reponse.three}</p>
+            <p>{el.reponse.four}</p>
+          </div>
+        </div>
+      );
+    });
+    console.log('questions', questions[0]);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <div classname="container">
+        <div className="container">
           <button />
-          <div />
-          <button />
+          <div>
+            {quizz}
+          </ div>
+          <button onClick={this.increment}>></button>
         </div>
       </div>
     );
---
+++ src/questions.js
@@ -0,0 +1,20 @@
+export const questions = [
+    {
+        question: "Comment s'appelle la startup qui permet de réexpédier les colis vers l'outremer ?",
+        reponse: {
+            one: "Livrez-moi",
+            two: "Livraison dans les îles",
+            three: "ShopDesIles",
+            four: "Shopîles"
+        }
+    },
+    {
+        question: "Comment s'appelle l'association des startups en Guadeloupe' ?",
+        reponse: {
+            one: "TechGuadeloupe",
+            two: "GuadeloupeTech",
+            three: "AssosGeekGuadeloupe",
+            four: "GeekGuadeloupe"
+        }
+    },
+]
